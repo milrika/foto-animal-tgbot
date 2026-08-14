6427f5db1c7acedfbdc72bb2caa7eb4f3ef0328d
milrika <qirimy@yandex.ru>
2026-08-14 22:14:45 +0500

    feat: добавлена кнопка котеки

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import telebot
 from dotenv import load_dotenv
 import os
+from services import get_cat_img
 
 load_dotenv()
 API_TOKEN = os.environ.get('API_TOKEN')
 
 bot = telebot.TeleBot(API_TOKEN)
 
+def get_keyboard():
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard = True)
+    btn_cat = telebot.types.KeyboardButton('котеки')
+    markup.add(btn_cat)
+
+    return markup
+    
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, 'привет я попа')
+    bot.send_message(message.chat.id, 'prive', reply_markup=get_keyboard())
 
-bot.infinity_polling()
+bot.infinity_polling(skip_pending=True)
