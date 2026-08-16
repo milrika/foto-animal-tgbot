9fe442849c2cd776d3a9fc22e3609234b8b80069
milrika <qirimy@yandex.ru>
2026-08-16 21:25:07 +0500

    feat: котеки отправляются

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 from dotenv import load_dotenv
 import os
 from services import get_cat_img
@@ -7,16 +8,20 @@ load_dotenv()
 API_TOKEN = os.environ.get('API_TOKEN')
 
 bot = telebot.TeleBot(API_TOKEN)
-
-def get_keyboard():
-    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard = True)
-    btn_cat = telebot.types.KeyboardButton('котеки')
-    markup.add(btn_cat)
-
-    return markup
     
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, 'prive', reply_markup=get_keyboard())
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    btn_cat = KeyboardButton('котеки')
+    markup.add(btn_cat)
+
+    bot.send_message(message.chat.id, 'prive', reply_markup=markup)
+
+@bot.message_handler(content_types=['text'])
+def send_img(message):
+    print('робит')
+    if message.text == 'котеки':
+        bot.send_photo(message.chat.id, get_cat_img(), caption='привет я котек!')
+
 
 bot.infinity_polling(skip_pending=True)
