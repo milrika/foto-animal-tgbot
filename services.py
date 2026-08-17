54f12f0a76dd97003307e83b289ec1f7b1471771
milrika <qirimy@yandex.ru>
2026-08-17 20:38:11 +0500

    feat: фотки уток

--- services.py
+++ services.py
@@ -1,6 +1,7 @@
 import requests
 
 CAT_API = 'https://api.thecatapi.com/v1/images/search'
+DUCK_API = 'https://random-d.uk/api/random'
 
 def get_cat_img():
     response = requests.get(CAT_API)
@@ -10,3 +11,12 @@ def get_cat_img():
     img_cat = data[0]['url']
 
     return img_cat
+
+def get_duck_img():
+    response = requests.get(DUCK_API)
+    response.raise_for_status()
+
+    data = response.json()
+    img_duck = data['url']
+
+    return img_duck
