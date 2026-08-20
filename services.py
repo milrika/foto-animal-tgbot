309175bcba973ec733f4a5f8ef104d229eb16ddc
milrika <qirimy@yandex.ru>
2026-08-20 18:27:14 +0500

    feat: добавлен TheDogAPI

--- services.py
+++ services.py
@@ -2,6 +2,8 @@ import requests
 
 CAT_API = 'https://api.thecatapi.com/v1/images/search'
 DUCK_API = 'https://random-d.uk/api/random'
+DOG_API = 'https://api.thedogapi.com/v1/images/search'
+
 
 def get_cat_img():
     response = requests.get(CAT_API)
@@ -20,3 +22,12 @@ def get_duck_img():
     img_duck = data['url']
 
     return img_duck
+
+def get_dog_api():
+    response = requests.get(DOG_API)
+    response.raise_for_status()
+
+    data = response.json()
+    img_dog = data[0]['url']
+
+    return img_dog
