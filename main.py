3fd435f427a7072d273df7339432b2b2bb67b995
milrika <qirimy@yandex.ru>
2026-08-20 18:33:01 +0500

    feat: добавлена отправка фото собак

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import telebot
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 from dotenv import load_dotenv
 import os
-from services import get_cat_img, get_duck_img
+from services import get_cat_img, get_duck_img, get_dog_api
 
 load_dotenv()
 API_TOKEN = os.environ.get('API_TOKEN')
@@ -14,16 +14,21 @@ def send_welcome(message):
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
     btn_cat = KeyboardButton('котеки')
     btn_duck = KeyboardButton('утке')
-    markup.add(btn_cat, btn_duck)
+    btn_dog = KeyboardButton('собако')
+    markup.add(btn_cat, btn_duck, btn_dog)
 
     bot.send_message(message.chat.id, 'prive', reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
 def send_img(message):
     if message.text == 'котеки':
-        bot.send_photo(message.chat.id, get_cat_img(), caption='привет я котек!')
-    else:
-        bot.send_photo(message.chat.id, get_duck_img(), 'привет я утка!')
+        bot.send_photo(message.chat.id, get_cat_img(), caption='привет, я котек!')
+
+    elif message.text == 'утке':
+        bot.send_photo(message.chat.id, get_duck_img(), 'привет, я утка!')
+
+    elif message.text == 'собако':
+        bot.send_photo(message.chat.id, get_dog_api(), 'привет, я собако!')
 
 
 bot.infinity_polling(skip_pending=True)
